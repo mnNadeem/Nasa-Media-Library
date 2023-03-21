3c93c8e4fca1a582ec9a8ed4e285e16b9acc6178
mnNadeem <muhammad.nadeem@devsinc.com>
2023-03-21 12:39:37 +0500

    added search and detail pages

---
+++ src/components/DetailsPage.js
@@ -0,0 +1,122 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import { Link, useLocation } from "react-router-dom";
+import styled from "styled-components";
+
+const Conatiner = styled.div`
+  display: flex;
+  margin-top: 100px;
+  justify-content: center;
+  @media (max-width: 700px) {
+    margin-top: 50px;
+    flex-direction: column;
+  }
+`;
+
+const LeftContainer = styled.div`
+  display: flex;
+  @media (max-width: 700px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+const RightContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-self: center;
+  width: 500px;
+  @media (max-width: 700px) {
+    margin-top: 20px;
+    width: 300px;
+  }
+`;
+
+const StyledImage = styled.img`
+  width: 300px;
+  height: 280px;
+  margin-right: 32px;
+  @media (max-width: 700px) {
+    margin-right: 0px;
+  }
+`;
+
+const Description = styled.div`
+  width: 500px;
+  height: 300px;
+  margin-right: 38px;
+  overflow-y: auto;
+  @media (max-width: 700px) {
+    width: 300px;
+    height: 99px;
+    margin-left: 0px;
+    margin-right: 0px;
+    overflow-y: scroll;
+    margin-top: 20px;
+  }
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 30px;
+  @media (max-width: 700px) {
+    margin-bottom: 30px;
+  }
+`;
+
+const DetailsPage = () => {
+  const {
+    state: { result },
+  } = useLocation();
+  const {
+    data: [
+      { title, description, photographer, location, keywords, date_created },
+    ],
+    links: [{ href }],
+  } = result;
+  return (
+    <>
+      <Conatiner>
+        <LeftContainer>
+          <StyledImage src={href} alt={title} />
+          <Description>
+            <b>Description:</b> {description}
+          </Description>
+        </LeftContainer>
+        <RightContainer>
+          {title && (
+            <p>
+              <b>Title:</b> {title}
+            </p>
+          )}
+          {photographer && (
+            <p>
+              <b>Photographer:</b> {photographer}
+            </p>
+          )}
+          {location && (
+            <p>
+              <b>Location:</b> {location}
+            </p>
+          )}
+          {keywords && (
+            <p>
+              <b>Keywords:</b> {keywords.join(" | ")}
+            </p>
+          )}
+          <p>
+            <b>Date:</b> {new Date(date_created).toLocaleDateString()}
+          </p>
+        </RightContainer>
+      </Conatiner>
+      <ButtonContainer>
+        <Link to="/">
+          <Button>Go Back to Search Page</Button>
+        </Link>
+      </ButtonContainer>
+    </>
+  );
+};
+
+export default DetailsPage;
---
+++ src/components/SearchPage.js
@@ -0,0 +1,227 @@
+import React, { useState } from "react";
+import styled, { keyframes } from "styled-components";
+import Alert from "react-bootstrap/Alert";
+import { Button } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
+
+const CardContainer = styled.div`
+  margin-top: 3rem;
+  margin-bottom: 3rem;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  gap: 3rem;
+
+  @media (max-width: 749px) {
+    justify-content: center;
+  }
+`;
+
+const Heading = styled.h1`
+  font-family: "Times New Roman", Times, serif;
+  font-weight: bold;
+  font-size: 32px;
+  line-height: 46px;
+  text-align: center;
+  color: #3944d7;
+  margin: 40px 20px;
+`;
+
+const SearchTextInput = styled.input`
+  border: 1px solid;
+  margin-top: 5px;
+  border-radius: 5px;
+  font-size: 16px;
+  height: 35px;
+  width: 100%;
+  padding: 10px 20px;
+  outline: none;
+  line-height: 22px;
+  :hover {
+    border: 2px solid #3944d7;
+  }
+`;
+
+const TextFielsContainer = styled.div`
+  margin-top: 32px;
+  display: flex;
+  justify-content: space-between;
+  @media (max-width: 749px) {
+    margin: 32px 27px 16px 27px;
+    flex-direction: column;
+  }
+`;
+
+const CardImage = styled(Card.Img)`
+  width: 100%;
+  height: 18rem;
+  object-fit: cover;
+`;
+
+const ErrorsContainer = styled.div`
+  margin-top: 20px;
+  margin-bottom: 20px;
+`;
+
+const SearchButton = styled(Button)`
+  margin-top: 20px;
+  @media (max-width: 749px) {
+    margin-left: 27px;
+  }
+`;
+
+const LoaderWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const spin = keyframes`
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Loader = styled.div`
+  border: 8px solid #f3f3f3;
+  border-top: 8px solid #3498db;
+  border-radius: 50%;
+  width: 80px;
+  height: 80px;
+  animation: ${spin} 2s linear infinite;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`;
+
+const Search = () => {
+  const [query, setQuery] = useState("");
+  const [yearStart, setYearStart] = useState("");
+  const [yearEnd, setYearEnd] = useState("");
+  const [results, setResults] = useState([]);
+  const [show, setShow] = useState(true);
+  const [responseError, setResponseError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const clearInputFields = () => {
+    setQuery("");
+    setYearEnd("");
+    setYearStart("");
+  };
+  const handleSearch = async () => {
+    try {
+      setShow(false);
+      setIsLoading(true);
+      const response = await fetch(
+        `https://images-api.nasa.gov/search?q=${encodeURIComponent(query)}${
+          yearStart ? `&year_start=${yearStart}` : ""
+        }${yearEnd ? `&year_end=${yearEnd}` : ""}&media_type=image`
+      );
+      const data = await response.json();
+      updateResults(data);
+    } catch (error) {
+      handleError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const updateResults = (data) => {
+    const { items } = data.collection;
+    if (items.length === 0) {
+      setResponseError("No data available for this search! Reset Your Search");
+      setShow(true);
+    }
+    setResults(items);
+    clearInputFields();
+  };
+
+  const handleError = (error) => {
+    if (error.response) {
+      setResponseError(error.response.data.reason || error.message);
+      setShow(true);
+    } else {
+      setResponseError("Something went wrong. Please try again later.");
+      setShow(true);
+    }
+  };
+
+  return (
+    <div className="container">
+      <Heading>Search NASA Media Library</Heading>
+      {responseError && show && (
+        <ErrorsContainer>
+          <Alert variant="danger" onClose={() => setShow(false)} dismissible>
+            <Alert.Heading>{responseError}</Alert.Heading>
+          </Alert>
+        </ErrorsContainer>
+      )}
+      <TextFielsContainer>
+        <label>
+          Query:
+          <SearchTextInput
+            type="text"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </label>
+        <label>
+          Start Year:
+          <SearchTextInput
+            type="text"
+            value={yearStart}
+            onChange={(e) => setYearStart(e.target.value)}
+          />
+        </label>
+        <label>
+          End Year:
+          <SearchTextInput
+            type="text"
+            value={yearEnd}
+            onChange={(e) => setYearEnd(e.target.value)}
+          />
+        </label>
+      </TextFielsContainer>
+      <SearchButton disabled={!query} onClick={handleSearch}>
+        Search
+      </SearchButton>
+      {isLoading ? (
+        <LoaderWrapper>
+          <Loader />
+        </LoaderWrapper>
+      ) : (
+        <CardContainer>
+          {results.map((result) => (
+            <Card style={{ width: "18rem" }} key={result?.data[0]?.nasa_id}>
+              <StyledLink to="/detail" state={{ result }}>
+                <CardImage
+                  variant="top"
+                  src={result?.links[0]?.href}
+                  alt={result?.data[0]?.title}
+                />
+              </StyledLink>
+              <Card.Body>
+                <Card.Title>
+                  {result?.data[0]?.title?.substring(0, 50)}
+                </Card.Title>
+                {result?.data[0]?.location && (
+                  <Card.Text>Location: {result?.data[0]?.location} </Card.Text>
+                )}
+                {result.data[0].photographer && (
+                  <Card.Text>
+                    Photographer: {result?.data[0]?.photographer}{" "}
+                  </Card.Text>
+                )}
+              </Card.Body>
+            </Card>
+          ))}
+        </CardContainer>
+      )}
+    </div>
+  );
+};
+
+export default Search;
